f5dd0373872f6579d3a042b36e7ecff896e0d42f
AfsanaSiddika <afsanaesha584@gmail.com>
2024-10-30 01:10:05 +0600

    modified the bid class

--- platforms/models.py
+++ platforms/models.py
@@ -34,17 +34,6 @@ class Platform(models.Model):
 
 
 
-class Bid(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    property = models.ForeignKey(Platform, on_delete=models.CASCADE, related_name='bids')
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    bid_status = models.CharField(max_length=20, choices=[
-        ('Pending', 'Pending'),
-        ('Accepted', 'Accepted'),
-        ('Rejected', 'Rejected')
-    ], default='Pending')
-    date_placed = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return f"Bid of {self.amount} on {self.property} by {self.user}"
 

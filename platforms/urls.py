9e2c972466822e82977f0f73ba46574daace1531
AfsanaSiddika <afsanaesha584@gmail.com>
2024-10-30 00:46:54 +0600

    added url paths

--- platforms/urls.py
+++ platforms/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.add_platform, name='add-platform'),
     path('update-platform/<int:p_id>', views.update_platform, name='update-platform'),
-    path('delete-platform/<int:p_id>', views.delete_platform, name='delete-platform'),
 ]
